67f4282265a5d9a292a6eab6521b4a4bac3b7206
Sf-Shohag71 <farid.192002038@green.ac.bd>
2023-04-09 02:59:44 +0600

    Implemented contextAPI

--- src/components/Brother/Brother.jsx
+++ src/components/Brother/Brother.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MoneyContext, ringContext } from '../Grandpa/Grandpa';
 
 const Brother = () => {
+    const [money, setMoney] = useContext(MoneyContext);
+    const Ring = useContext(ringContext);
     return (
         <div>
             <h4>Brother</h4>
+            <p><small>Grandpa's Money: {money}</small></p>
+            <button onClick={() => setMoney(money+1000)}>Send 1k</button>
+            <p><small>{Ring}</small></p>
         </div>
-    );
+    )
 };
 
 export default Brother;
--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,21 +1,26 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import Father from "../Father/Father";
 import Uncle from "../Uncle/Uncle";
 import Aunty from "../Aunty/Aunty";
 import "./Grandpa.css";
 
 export const ringContext = createContext("Gold");
+export const MoneyContext = createContext(0);
 const Grandpa = () => {
+  const [money, setMoney] = useState(0);
   return (
     <div className="grandpa">
       <h3>Grandpa</h3>
-      <ringContext.Provider value="Golden Ring">
-        <section className="flex">
-          <Father></Father>
-          <Uncle></Uncle>
-          <Aunty></Aunty>
-        </section>
-      </ringContext.Provider>
+      <p>Has Money: {money}</p>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <ringContext.Provider value="Golden Ring">
+          <section className="flex">
+            <Father></Father>
+            <Uncle></Uncle>
+            <Aunty></Aunty>
+          </section>
+        </ringContext.Provider>
+      </MoneyContext.Provider>
     </div>
   );
 };
